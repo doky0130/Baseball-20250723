6530cdc6b3848a85fc54951b94b78e3a2c711676
Kim Dokyoung <doky0130@gmail.com>
2025-07-23 13:03:08 +0900

    [feature] page 30

--- 20230723_BaseballKATA/baseball_test.cpp
+++ 20230723_BaseballKATA/baseball_test.cpp
@@ -1,9 +1,14 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
+TEST(BaseballGame, ThrowLenException) {
 	Baseball game;
 	EXPECT_THROW(game.guess("12"), length_error);
+}
+
+TEST(BaseballGame, ThrowInvalidCharException) {
+	Baseball game;
+	EXPECT_THROW(game.guess("12s"), invalid_argument);
 }
 
 int main() {
