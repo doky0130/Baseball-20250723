80b9195c7eeb7f64f88a68efb844c477c7a1b088
Kim Dokyoung <doky0130@gmail.com>
2025-07-23 13:25:31 +0900

    [feature] page 49

--- 20230723_BaseballKATA/baseball_test.cpp
+++ 20230723_BaseballKATA/baseball_test.cpp
@@ -3,14 +3,14 @@
 
 class BaseballFixture : public testing::Test {
 public:
-	Baseball game;
+	Baseball game{ "123" };
 	void assertionIllegalArgument(const string& string) {
 		try {
 			game.guess(string);
 			FAIL();
 		}
 		catch (const exception& e) {
-
+			
 		}
 	}
 };
@@ -19,6 +19,14 @@ TEST_F(BaseballFixture, ThrowExceptionInvalidArgument) {
 	assertionIllegalArgument("12");
 	assertionIllegalArgument("12s");
 	assertionIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+	
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
 
 int main() {
